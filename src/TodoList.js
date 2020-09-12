6c7c71183d1637ec57c9439672c8a5e7f168a1b4
kakizakimaki <xxxmaki112@gmail.com>
2020-09-12 20:28:06 +0900

    ListComponentの作成

---
+++ src/TodoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+function TodoList() {
+  return (
+    <List>
+      <ListItem>
+        <ListItemText primary="プログラミング" />
+      </ListItem>
+      <ListItem>
+        <ListItemText primary="お買い物" />
+      </ListItem>
+    </List>
+
+  );
+}
+
+export default TodoList;
