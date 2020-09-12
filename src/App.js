396bea0c4fbe0964cfe0cf7543bfc4ff09dbac3a
kakizakimaki <xxxmaki112@gmail.com>
2020-09-12 22:02:45 +0900

    todoに遷移できるよう修正

--- src/App.js
+++ src/App.js
@@ -1,51 +1,5 @@
-// import React from 'react';
-// import TodoList from './TodoList';
-// import Form from './Form';
-// import './App.css';
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       list: ['ごはん食べる', 'お買い物'],
-//       value: '',
-//     };
-//   }
-
-//   handleChange = (event) => {
-//     this.setState({ value: event.target.value });
-//   }
-
-//   handleSubmit = (event) => {
-//     event.preventDefault();
-
-//     this.setState({
-//       list: [
-//         ...this.state.list,
-//         this.state.value,
-//       ],
-//       value: '',
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div className="app">
-//         <h1>TODOアプリ</h1>
-//         <Form
-//           value={this.state.value}
-//           handleChange={this.handleChange}
-//           handleSubmit={this.handleSubmit}
-//         />
-//         <TodoList list={this.state.list} />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-import React, { Component } from 'react'
+import React from 'react';
+import Todos from './Todos';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 const App = () => (
@@ -54,7 +8,7 @@ const App = () => (
       <ul>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/about'>About</Link></li>
-        <li><Link to='/friends'>Todo</Link></li>
+        <li><Link to='/todo'>Todo</Link></li>
       </ul>
       <hr />
       <Route exact path='/' component={Home} />
@@ -79,6 +33,7 @@ const About = () => (
 const Todo = () => (
   <div>
     <h2>Todo</h2>
+    <Todos />
   </div>
 )
 
---
+++ src/Todos.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import TodoList from './TodoList';
+import Form from './Form';
+import './App.css';
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: ['ごはん食べる', 'お買い物'],
+      value: '',
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      list: [
+        ...this.state.list,
+        this.state.value,
+      ],
+      value: '',
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>TODOアプリ</h1>
+        <Form
+          value={this.state.value}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <TodoList list={this.state.list} />
+      </div>
+    );
+  }
+}
+
+export default Todo;
